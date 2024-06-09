b50a507ae4efb194ddd50f52b7b23ff767f6f9d0
mikesplore <mikepremium8@gmail.com>
2024-06-09 15:55:54 +0300

    My class will start here

--- app/src/main/java/com/mike/myclass/ui/theme/Theme.kt
+++ app/src/main/java/com/mike/myclass/ui/theme/Theme.kt
@@ -10,6 +10,11 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.mike.myclass.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -31,6 +36,10 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
+)
+val RobotoMono = FontFamily(
+    Font(R.font.segoepr, FontWeight.Normal),
+
 )
 
 @Composable
