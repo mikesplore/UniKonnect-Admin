5b10b50ed59c6db6475533b3d8c636c0d40e8805
mikesplore <mikepremium8@gmail.com>
2024-06-09 18:51:33 +0300

    My class will start here

---
+++ app/src/main/java/com/mike/myclass/ManageUsers.kt
@@ -0,0 +1,156 @@
+package com.mike.myclass
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.Coil
+import coil.compose.rememberAsyncImagePainter
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import com.mike.myclass.CommonComponents as CC
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ManageUsers(navController: NavController){
+    var users by remember { mutableStateOf<List<User>?>(null) }
+    var loading by remember { mutableStateOf(true) }
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        MyDatabase.getUsers { fetchedUsers ->
+            users = fetchedUsers
+            loading = false
+        }
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        Icons.Default.ArrowBackIosNew, "back", tint = GlobalColors.textColor
+                    )
+                    }
+            }, title = { Text("Manage Users", style = CC.titleTextStyle) }, colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = GlobalColors.primaryColor,
+                titleContentColor = GlobalColors.textColor
+            ))
+        },
+        containerColor = GlobalColors.primaryColor
+    ) {
+        Column(modifier = Modifier
+            .background(CC.backbrush)
+            .fillMaxWidth()
+            .padding(it)) {
+            if(loading){
+                Column(modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally){
+                    CircularProgressIndicator(color = GlobalColors.textColor)
+                    Text("Users Loading...")
+                }
+                }
+            Row(
+                modifier = Modifier
+                    .height(50.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Name", style = CC.descriptionTextStyle)
+                Text("Email", style = CC.descriptionTextStyle)
+            }
+            users?.forEach { user ->
+                RowImages(
+                    user = user,
+                    route = "profile",
+                    navController = navController
+                )
+            }
+
+        }
+
+    }
+
+}
+
+
+@Composable
+fun RowImages(user:User, route:String, navController: NavController){
+    Row(
+        modifier = Modifier
+
+            .height(50.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(user.name, style = CC.descriptionTextStyle)
+        Text(user.email, style = CC.descriptionTextStyle)
+
+    }
+    HorizontalDivider(color = GlobalColors.textColor)
+}
+
+@Preview
+@Composable
+fun ManagementUsersProfile(){
+    ManageUsers(navController = rememberNavController())
+
+}
