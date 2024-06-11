df5f7a9657ca2e28b0271f5dc7db275676d86aa7
mikesplore <mikepremium8@gmail.com>
2024-06-11 19:07:14 +0300

    continuous

---
+++ app/src/main/java/com/mike/myclass/CustomColors.kt
@@ -0,0 +1,16 @@
+package com.mike.myclass
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CustomColors(){
+    Column {  }
+
+}
+@Preview
+@Composable
+fun MyColorPreview(){
+    CustomColors()
+}
