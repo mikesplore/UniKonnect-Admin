9c06451c301786db721c3d3d00b9317608162036
mikesplore <mikepremium8@gmail.com>
2024-06-14 23:55:38 +0300

    Continuation

--- app/src/main/java/com/mike/myclass/ManageUsers.kt
+++ app/src/main/java/com/mike/myclass/ManageUsers.kt
@@ -1,6 +1,7 @@
 package com.mike.myclass
 
 import android.content.Context
+import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
@@ -18,13 +19,24 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -35,6 +47,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,7 +63,7 @@ fun ManageUsers(navController: NavController) {
     var loading by remember { mutableStateOf(true) }
     val context = LocalContext.current
 
-    LaunchedEffect(Unit) {
+    LaunchedEffect(loading) {
         MyDatabase.getUsers { fetchedUsers ->
             users = fetchedUsers
             loading = false
@@ -71,6 +84,11 @@ fun ManageUsers(navController: NavController) {
         Scaffold(
             topBar = {
                 TopAppBar(
+                    actions = {
+                        IconButton(onClick = {loading = true}) { 
+                            Icon(Icons.Default.Refresh,"refresh", tint = GlobalColors.textColor)
+                        }
+                    },
                     navigationIcon = {
                         IconButton(onClick = { navController.popBackStack() }) {
                             Icon(
@@ -106,6 +124,7 @@ fun ManageUsers(navController: NavController) {
                         Text("Users Loading...", style = CC.descriptionTextStyle(context))
                     }
                 } else {
+
                     Column {
                         Row(
                             modifier = Modifier
@@ -118,18 +137,19 @@ fun ManageUsers(navController: NavController) {
                             Text("Name", style = CC.descriptionTextStyle(context), fontWeight = FontWeight.Bold)
                             Text("Email", style = CC.descriptionTextStyle(context), fontWeight = FontWeight.Bold)
                         }
-                        val total = users?.size
                         HorizontalDivider(color = GlobalColors.textColor)
                         users?.forEach { user ->
-                            AnimatedUserRow(
+                            UserCard(
                                 user = user,
                                 context = context,
-                                navController = navController
                             )
                         }
-                        Row(modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center) {  
-                            Text("Total Users: $total", style = CC. descriptionTextStyle(context))
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text("Total Users: ${users?.size ?: 0}", style = CC.descriptionTextStyle(context))
                         }
                     }
                 }
@@ -138,37 +158,141 @@ fun ManageUsers(navController: NavController) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AnimatedUserRow(user: User, context: Context, navController: NavController) {
+fun UserCard(user: User, context: Context) {
     var expanded by remember { mutableStateOf(false) }
+    var name by remember { mutableStateOf(user.name) }
+    var isediting by remember { mutableStateOf(false) }
+    if(isediting){
+        BasicAlertDialog(onDismissRequest = {isediting  = false}) {
+            Column(modifier = Modifier
+                .background(CC.backbrush, RoundedCornerShape(10.dp))
+                .width(300.dp)
+                .height(200.dp),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center) {
+                    Text("Edit User Name", style = CC.titleTextStyle(context),
+                        modifier = Modifier.padding(top = 10.dp))
+                }
+                OutlinedTextField(
+                    value = name,
+                    textStyle = CC.descriptionTextStyle(context = LocalContext.current),
+                    onValueChange = {name = it},
+                    label = { Text("Name", style = CC.descriptionTextStyle(context = LocalContext.current)) },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(0.9f),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = GlobalColors.primaryColor,
+                        unfocusedLabelColor = GlobalColors.tertiaryColor,
+                        focusedIndicatorColor = GlobalColors.textColor,
+                        unfocusedContainerColor = Color.Transparent,
+                        unfocusedTextColor = GlobalColors.textColor,
+                        focusedTextColor = GlobalColors.textColor,
+                        focusedLabelColor = GlobalColors.primaryColor,
+                        unfocusedIndicatorColor = GlobalColors.textColor
+                    ),
+                    singleLine = true
+                )
+                Row(modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround) {
+                    Button(onClick = {
 
-    Column(
+                        MyDatabase.updateUser(user.id, name,
+                            onSuccess = {
+                                // Show success message or perform other actions
+                                Toast.makeText(context, "User name updated successfully!", Toast.LENGTH_SHORT).show()
+                                isediting = false
+                            },
+                            onFailure = { exception ->
+                                // Handle the failure, e.g., display an error message
+                                if (exception != null) {
+                                    Toast.makeText(context, "Error updating user name: ${exception.message}", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        )
+
+                    },
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = GlobalColors.primaryColor,
+                            contentColor = GlobalColors.textColor
+                        )
+                    ) {
+                        Text("Save", style = CC.descriptionTextStyle(context))
+                    }
+                    Button(onClick = { isediting = false},
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = GlobalColors.primaryColor,
+                            contentColor = GlobalColors.textColor
+                        )
+                    ) {
+                        Text("Cancel", style = CC.descriptionTextStyle(context))
+                    }
+                }
+            }
+        }
+    }
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { expanded = !expanded }
-            .padding(8.dp)
-            .background(GlobalColors.primaryColor, shape = RoundedCornerShape(8.dp))
-            .padding(16.dp)
+            .padding(vertical = 8.dp)
+            .clickable { expanded = !expanded },
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(GlobalColors.primaryColor)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         ) {
-            Text(user.name, style = CC.descriptionTextStyle(context))
-            Text(user.email, style = CC.descriptionTextStyle(context))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(user.name, style = CC.descriptionTextStyle(context))
+                Text(user.email, style = CC.descriptionTextStyle(context))
+                Row {
+                    IconButton(onClick = {isediting= true}) {
+                        Icon(Icons.Default.Edit, contentDescription = "Edit", tint = GlobalColors.textColor)
+                    }
+                    IconButton(onClick = {
+                        MyDatabase.deleteUser(user.id,
+                            onSuccess = {
+                                // Show success message or perform other actions
+                                Toast.makeText(context, "User deleted successfully!", Toast.LENGTH_SHORT).show()
+                            },
+                            onFailure = { exception ->
+                                // Handle the failure, e.g., display an error message
+                                if (exception != null) {
+                                    Toast.makeText(context, "Error deleting user: ${exception.message}", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        )
+                    }) {
+                        Icon(Icons.Default.Delete, contentDescription = "Delete", tint = GlobalColors.textColor)
+                    }
+                }
+            }
+            if (expanded) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Additional user details can go here.", style = CC.descriptionTextStyle(context))
+            }
         }
-        if (expanded) {
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("Additional user details can go here.", style = CC.descriptionTextStyle(context))
-        }
-        HorizontalDivider(color = GlobalColors.textColor)
     }
-
 }
 
 @Preview
 @Composable
 fun ManagementUsersProfile() {
     ManageUsers(navController = rememberNavController())
+
 }
