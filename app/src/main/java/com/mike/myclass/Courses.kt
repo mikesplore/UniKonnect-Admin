f1fe7fa54bc8f09cd9b84eafe1aca741c3b02272
mikesplore <mikepremium8@gmail.com>
2024-06-22 17:09:33 +0300

    Continuation

--- app/src/main/java/com/mike/myclass/Courses.kt
+++ app/src/main/java/com/mike/myclass/Courses.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
@@ -41,22 +42,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.mike.myclass.CommonComponents as CC
 
 data class Course(
-    val courseCode: String = "",
-    val courseName: String = ""
+    val courseCode: String = "", val courseName: String = ""
 
 )
 
@@ -64,7 +60,7 @@ data class Course(
 @Composable
 fun CoursesScreen(navController: NavController, context: Context) {
     val courses = remember { mutableStateListOf<Course>() }
-    var showAddDialog by remember { mutableStateOf(false) }
+    var showAddDialog by remember { mutableStateOf(true) }
     var loading by remember { mutableStateOf(true) }
 
     LaunchedEffect(loading) {
@@ -78,31 +74,28 @@ fun CoursesScreen(navController: NavController, context: Context) {
 
 
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
+    Scaffold(topBar = {
+        TopAppBar(
 
-                title = { Text("Courses") },
-                actions = {
-                    IconButton(onClick = {
-                        loading = true
-                    }) {
-                        Icon(Icons.Default.Refresh,"refresh",
-                            tint = GlobalColors.textColor)
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = GlobalColors.primaryColor,
-                    titleContentColor = GlobalColors.textColor)
+            title = { Text("Courses") }, actions = {
+                IconButton(onClick = {
+                    loading = true
+                }) {
+                    Icon(
+                        Icons.Default.Refresh, "refresh", tint = GlobalColors.textColor
+                    )
+                }
+            }, colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = GlobalColors.primaryColor,
+                titleContentColor = GlobalColors.textColor
             )
+        )
 
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { showAddDialog = true }) {
-                Icon(painterResource(id = android.R.drawable.ic_input_add), contentDescription = "Add Course")
-            }
-        },
-        containerColor = GlobalColors.primaryColor
+    }, floatingActionButton = {
+        FloatingActionButton(onClick = { showAddDialog = true }) {
+            Icon(Icons.Default.Add, contentDescription = "Add Course")
+        }
+    }, containerColor = GlobalColors.primaryColor
     ) {
         Column(
             modifier = Modifier
@@ -110,20 +103,18 @@ fun CoursesScreen(navController: NavController, context: Context) {
                 .background(GlobalColors.primaryColor)
                 .padding(it)
         ) {
-            if(loading){
+            if (loading) {
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     CircularProgressIndicator(
-                        color = GlobalColors.primaryColor,
-                        trackColor = GlobalColors.textColor
+                        color = GlobalColors.primaryColor, trackColor = GlobalColors.textColor
                     )
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(
-                        text = "Loading...",
-                        color = GlobalColors.textColor
+                        text = "Loading...", color = GlobalColors.textColor
                     )
                 }
 
@@ -132,32 +123,28 @@ fun CoursesScreen(navController: NavController, context: Context) {
                 Row(
                     modifier = Modifier
                         .border(
-                            width = 1.dp,
-                            color = Color.Gray,
-                            shape = RoundedCornerShape(10.dp)
+                            width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(10.dp)
                         )
                         .fillMaxWidth()
                         .padding(16.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
-                        text = course.courseCode,
-                        color = Color.White,
-                        fontSize = 18.sp
+                        text = course.courseCode, color = Color.White, fontSize = 18.sp
                     )
                     Spacer(modifier = Modifier.width(16.dp))
                     Text(
-                        text = course.courseName,
-                        color = Color.White,
-                        fontSize = 18.sp
+                        text = course.courseName, color = Color.White, fontSize = 18.sp
                     )
                     Spacer(modifier = Modifier.weight(1f))
                     IconButton(onClick = {
                         navController.navigate("course/${course.courseCode}")
                     }) {
-                        Icon(Icons.AutoMirrored.Filled.ArrowForwardIos,
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowForwardIos,
                             contentDescription = "View Course",
-                            tint = Color.White)
+                            tint = Color.White
+                        )
                     }
 
                 }
@@ -166,17 +153,14 @@ fun CoursesScreen(navController: NavController, context: Context) {
     }
 
     if (showAddDialog) {
-        AddCourseDialog(
-            onDismiss = { showAddDialog = false },
+        AddCourseDialog(onDismiss = { showAddDialog = false },
             onAddCourse = { courseCode, courseName ->
                 val newCourse = Course(courseCode, courseName)
                 val database = Firebase.database.reference.child("Courses").child(courseCode)
-                database.setValue(newCourse)
-                    .addOnSuccessListener {
+                database.setValue(newCourse).addOnSuccessListener {
                         courses.add(newCourse)
                         showAddDialog = false
-                    }
-                    .addOnFailureListener { exception ->
+                    }.addOnFailureListener { exception ->
                         // Handle the error
                     }
             },
@@ -189,60 +173,45 @@ fun CoursesScreen(navController: NavController, context: Context) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddCourseDialog(onDismiss: () -> Unit, onAddCourse: (String, String) -> Unit, context: Context) {
+fun AddCourseDialog(
+    onDismiss: () -> Unit,
+    onAddCourse: (String, String) -> Unit,
+    context: Context
+) {
     var courseCode by remember { mutableStateOf("") }
     var courseName by remember { mutableStateOf("") }
 
     BasicAlertDialog(
-        onDismissRequest = onDismiss,
-        modifier = Modifier.width(300.dp) // Set width for the dialog
+        onDismissRequest = onDismiss, modifier = Modifier.width(300.dp) // Set width for the dialog
     ) {
         Column(
             modifier = Modifier
                 .border(
-                    width = 1.dp,
-                    color = Color.Gray,
-                    shape = RoundedCornerShape(10.dp)
+                    width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(10.dp)
                 )
                 .padding(16.dp)
         ) {
             Text(
-                "Add New Course",
-                style = CC.titleTextStyle(LocalContext.current)
+                "Add New Course", style = CC.titleTextStyle(LocalContext.current)
             )
             Spacer(modifier = Modifier.height(16.dp))
-            TextField(
+            InputDialogTextField(
                 value = courseCode,
                 onValueChange = { courseCode = it },
-                label = { Text("Course Code", style = CC.descriptionTextStyle(context)) },
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = GlobalColors.tertiaryColor,
-                    unfocusedIndicatorColor = GlobalColors.secondaryColor,
-                    focusedTextColor = GlobalColors.textColor,
-                    unfocusedTextColor = GlobalColors.textColor,
-                    focusedContainerColor = GlobalColors.secondaryColor,
-                    unfocusedContainerColor = GlobalColors.secondaryColor
-                )
+                label = "Course Code",
+                context
             )
             Spacer(modifier = Modifier.height(8.dp))
-            TextField(
+            InputDialogTextField(
                 value = courseName,
                 onValueChange = { courseName = it },
-                label = { Text("Course Name", style = CC.descriptionTextStyle(context)) },
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = GlobalColors.tertiaryColor,
-                    unfocusedIndicatorColor = GlobalColors.secondaryColor,
-                    focusedTextColor = GlobalColors.textColor,
-                    unfocusedTextColor = GlobalColors.textColor,
-                    focusedContainerColor = GlobalColors.secondaryColor,
-                    unfocusedContainerColor = GlobalColors.secondaryColor
-                )
+                label = "Course Name",
+                context
             )
             Spacer(modifier = Modifier.height(16.dp))
             // Row with two buttons
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 Button(
                     onClick = onDismiss,
