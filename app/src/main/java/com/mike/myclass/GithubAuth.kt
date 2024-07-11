5fd8c57102ce4497e0080f9d68253e9f933561a2
mikesplore <mikepremium8@gmail.com>
2024-07-12 01:31:18 +0300

    commit

--- app/src/main/java/com/mike/myclass/GithubAuth.kt
+++ app/src/main/java/com/mike/myclass/GithubAuth.kt
@@ -2,7 +2,6 @@ package com.mike.myclass
 
 import android.app.Activity
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -20,24 +19,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.OAuthProvider
-
-// Data class for User (outside of any composable)
+import com.mike.myclass.CommonComponents as CC
 
 @Composable
 fun GitAuth(
     firebaseAuth: FirebaseAuth,
     onSignInSuccess: () -> Unit,
     onSignInFailure: (String) -> Unit,
-    navController: NavController
 ) {
-    val context = LocalContext.current
     val activity = LocalContext.current as Activity
     val provider = OAuthProvider.newBuilder("github.com")
     var isLoading by remember { mutableStateOf(false) } // State to track loading
@@ -49,10 +43,8 @@ fun GitAuth(
                 firebaseAuth
                     .startActivityForSignInWithProvider(activity, provider.build())
                     .addOnSuccessListener {
-                        // Navigate to more details screen to add details
                         isLoading = false // Stop loading on success
                         onSignInSuccess()
-                        navController.navigate("moredetails")
 
                     }
                     .addOnFailureListener {
@@ -63,10 +55,10 @@ fun GitAuth(
             }
             .border(
                 width = 1.dp,
-                color = GlobalColors.textColor,
+                color = CC.textColor(),
                 shape = RoundedCornerShape(10.dp)
             )
-            .background(GlobalColors.secondaryColor, shape = RoundedCornerShape(10.dp))
+            .background(CC.secondary(), shape = RoundedCornerShape(10.dp))
             .height(60.dp)
             .width(130.dp),
         contentAlignment = Alignment.Center
@@ -75,8 +67,8 @@ fun GitAuth(
             // Show CircularProgressIndicator when loading
             CircularProgressIndicator(
                 modifier = Modifier.size(30.dp),
-                color = GlobalColors.primaryColor,
-                trackColor = GlobalColors.textColor
+                color = CC.primary(),
+                trackColor = CC.textColor()
 
             )
         } else {
